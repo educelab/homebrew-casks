2a060dad47e7823c140808373428d94d867b8829
Seth Parker <csparker247@gmail.com>
2023-03-20 16:23:39 -0400

    Update volume-cartographer from 2.24.0 to 2.24.1

--- Casks/volume-cartographer.rb
+++ Casks/volume-cartographer.rb
@@ -2,9 +2,9 @@ cask "volume-cartographer" do
   arch arm:   "arm64",
        intel: "x86_64"
 
-  version "2.24.0"
-  sha256 arm:   "33abb1170522cb3ebc89753eb2d2352b4723492ede3fb6b77549642c8a165389",
-         intel: "2487ad797c207033b9140d1fd788ef5f74fc14fef2a6a840a61a433006e61458"
+  version "2.24.1"
+  sha256 arm:   "0b2a9a0f7108639f28dccac7aa286777faaea45f895a92d5b9ee0a61c75e2166",
+         intel: "c0e862c8093b5839584ba5752cab17ef68a1a5ddb86a0675fe89483a4d3d86b9"
 
   url "https://github.com/educelab/volume-cartographer/releases/download/v#{version}/VC-#{version}-Darwin-#{arch}.zip"
   name "Volume Cartographer"
