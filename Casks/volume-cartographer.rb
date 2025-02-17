bf167e7094dd7aa5e8a07ca6a66b0a33cb950b06
Seth Parker <csparker247@gmail.com>
2025-02-17 12:26:43 -0500

    Better macOS version test

--- Casks/volume-cartographer.rb
+++ Casks/volume-cartographer.rb
@@ -1,20 +1,19 @@
 cask "volume-cartographer" do
-  arch arm:   "arm64",
-       intel: "x86_64"
 
   version "2.27.0"
-  sha256 arm:   "6d4dea1aa21e145f2e1fc36f59d279151e028238d00449c63603197651b6c8db",
-         intel: "b8f9e270a306cbcf6327468356be913ff50d3ac70e31ac5b0157a0f750bb7419"
 
-  on_arm do
+  on_sonoma :or_newer do
     depends_on macos: ">= :sonoma"
-  end
-  on_intel do
-    depends_on macos: ">= :monterey"
+    url "https://github.com/educelab/volume-cartographer/releases/download/v#{version}/VC-#{version}-Darwin.zip"
+    sha256 "6d4dea1aa21e145f2e1fc36f59d279151e028238d00449c63603197651b6c8db"
   end
 
-  url_name = on_arch_conditional arm: "VC-#{version}-Darwin.zip", intel: "VC-#{version}-Darwin-macOS12.zip"
-  url "https://github.com/educelab/volume-cartographer/releases/download/v#{version}/#{url_name}"
+  on_ventura :or_older do
+    depends_on macos: ">= :monterey"
+    url "https://github.com/educelab/volume-cartographer/releases/download/v#{version}/VC-#{version}-Darwin-macOS12.zip"
+    sha256 "b8f9e270a306cbcf6327468356be913ff50d3ac70e31ac5b0157a0f750bb7419"
+  end
+
   name "Volume Cartographer"
   desc "Virtual unwrapping toolkit"
   homepage "https://github.com/educelab/volume-cartographer"
